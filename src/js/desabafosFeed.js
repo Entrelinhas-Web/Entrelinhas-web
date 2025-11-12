8c071349441d1e77c22466851664bb9a82fc6f09
Jairo <jairoccostajr@gmail.com>
2025-11-11 22:09:49 -0300

    refactor: Refatora a estrutura do feed de desabafos

---
+++ src/js/desabafosFeed.js
@@ -0,0 +1,7 @@
+import { menuBar } from "./scripts/menu.js";
+import { showRegistros } from "./scripts/registros.js";
+import bgParticles from "./scripts/bgParticles.js";
+
+bgParticles();
+menuBar();
+showRegistros();
