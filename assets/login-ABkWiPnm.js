246d4baec0087f5cbceae989acec7bec4e9f6fe2
Jairo <jairoccostajr@gmail.com>
2025-11-12 00:49:59 -0300

    Updates

--- assets/login-ABkWiPnm.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./bgParticles-B7DeQa9F.js";document.addEventListener("DOMContentLoaded",()=>{e()});
---
+++ assets/login-CdmjAWzg.js
@@ -0,0 +1,1 @@
+import{b as e}from"./bgParticles-CJLKLwgA.js";document.addEventListener("DOMContentLoaded",()=>{e()});
