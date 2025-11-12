598acf0581390abb23531ddf58a680ff7ac2f017
Vitor Igor <lessa.vitorigor@gmail.com>
2025-11-12 16:10:56 -0300

    Updates

--- assets/login-DbzRjujZ.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./bgParticles-B9ckXd_y.js";document.addEventListener("DOMContentLoaded",()=>{e()});
---
+++ assets/main-Uez__kXL.js
@@ -0,0 +1,1 @@
+import{b as e}from"./bgParticles-DdfI2ZwG.js";document.addEventListener("DOMContentLoaded",()=>{e()});
