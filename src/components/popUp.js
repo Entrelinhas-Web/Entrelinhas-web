ae7897577968e0550418f53a0d2bee0746663780
Vitor <lessa.vitorigor@gmail.com>
2025-11-17 11:15:40 -0300

    feat: Adiciona exclusão de desabafos

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -1,41 +1,40 @@
-const popUp = (modalId, objeto, border, text) => {
-
-    const starImg = `<img 
-                        src="../../public/star.png" 
+export default function popUp(modalId, objeto, border, text) {
+  const starImg = `<img 
+                        src="/Entrelinhas-web/star.png" 
                         alt="Estrela de intensidade" 
                         class="w-5 h-5"
                     >`;
 
-    const starsHtml = starImg.repeat(objeto.nivel || 0);
-    
-    return `
+  const starsHtml = starImg.repeat(objeto.nivel || 0);
+
+  return `
     <div 
         id="${modalId}" 
-        class="hidden fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/30"
+        class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/30"
     >
         <div 
             class="bg-preto p-6 rounded-2xl shadow-xl w-full max-w-5xl relative border-2 ${border}"
         >
             
-            <div class="absolute top-3 right-4 flex items-center gap-x-3">
+            <div class="absolute top-3 right-4 flex items-center gap-3 text-center">
                 
                 <button 
                     title="Editar"
-                    class="text-gray-400 hover:opacity-80 transition-opacity js-edit-popup"
+                    class="text-gray-400 hover:opacity-80 transition-opacity edit-popup"
                 >
-                    <img src="../../public/edit.png" alt="Editar" class="w-5 h-5">
+                    <img src="/Entrelinhas-web//edit.png" alt="Editar" class="w-5 h-5">
                 </button>
                 
                 <button 
                     title="Remover"
-                    class="text-gray-400 hover:opacity-80 transition-opacity js-remove-popup"
+                    class="text-gray-400 hover:opacity-80 transition-opacity remove-popup"
                 >
-                    <img src="../../public/remove.png" alt="Remover" class="w-5 h-5">
+                    <img src="/Entrelinhas-web/trash.png" alt="Remover" class="w-5 h-5">
                 </button>
 
                 <button 
                     title="Fechar"
-                    class="text-gray-400 hover:text-white text-2xl font-bold js-close-popup"
+                    class="text-gray-400 hover:text-white text-2xl font-bold close-popup pt-1"
                 >
                     &times;
                 </button>
@@ -57,5 +56,3 @@ const popUp = (modalId, objeto, border, text) => {
     </div>
     `;
 }
-
-export default popUp;
