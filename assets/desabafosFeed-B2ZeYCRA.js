e0a2e5de473b4e968855c81e0f402b9c4f5ce6ea
Vitor <lessa.vitorigor@gmail.com>
2025-11-17 10:59:12 -0300

    Updates

--- assets/desabafosFeed-B2ZeYCRA.js
+++ assets/desabafosFeed-B2ZeYCRA.js
@@ -1,4 +1,4 @@
-import{b}from"./bgParticles-DnaNUedT.js";function f(){const t=document.querySelector(".form");t.innerHTML="",t.insertAdjacentHTML("beforeend",`
+import{b as f}from"./bgParticles-DnaNUedT.js";function x(){const t=document.querySelector(".form");t.innerHTML="",t.insertAdjacentHTML("beforeend",`
   <div class="bg-roxo border-lilas border-4 w-[80%] lg:w-[55%] rounded-lg p-6 shadow-md mx-auto">
     <h1 class="mb-6 text-amarelo pb-5 text-center text-xl font-semibold">
       Adicionar desabafo
@@ -75,7 +75,7 @@ import{b}from"./bgParticles-DnaNUedT.js";function f(){const t=document.querySele
         </button>
       </div>
     </form>
-  </div>`),t.classList.remove("hidden"),t.classList.add("flex");const a=document.getElementById("nivel"),l=document.getElementById("valorNivel");a.addEventListener("input",()=>{l.textContent=`${a.value} / 5`})}function x(){document.querySelector("#app").insertAdjacentHTML("beforeend",`
+  </div>`),t.classList.remove("hidden"),t.classList.add("flex");const a=document.getElementById("nivel"),o=document.getElementById("valorNivel");a.addEventListener("input",()=>{o.textContent=`${a.value} / 5`})}function g(){document.querySelector("#app").insertAdjacentHTML("beforeend",`
     <div id="sidebar" class="bg-preto text-branco border-l-2 border-branco/80 fixed top-0 right-0 z-50 hidden h-full w-64 translate-x-full transform shadow-xl transition-all duration-300 ease-in-out">
         <button
         id="close-btn"
@@ -93,12 +93,12 @@ import{b}from"./bgParticles-DnaNUedT.js";function f(){const t=document.querySele
         </ul>
         </div>
     </div>
-  `)}function g(){const t=document.querySelector(".search");JSON.parse(localStorage.getItem("desabafos")||"[]").length>0&&(t.classList.remove("hidden"),t.classList.add("flex"));const a=t.querySelector("input");a.addEventListener("input",()=>{const l=(a.value||"").trim().toLowerCase(),s=new CustomEvent("state-change",{detail:{searchText:l}});window.dispatchEvent(s)})}const p={Felicidade:{bg:"bg-amarelo",border:"border-amarelo",text:"text-amarelo"},Tristeza:{bg:"bg-azul",border:"border-azul",text:"text-azul"},Raiva:{bg:"bg-vermelho",border:"border-vermelho",text:"text-vermelho"},Ansiedade:{bg:"bg-laranja",border:"border-laranja",text:"text-laranja"},Motivação:{bg:"bg-rosa",border:"border-rosa",text:"text-rosa"},Tranquilidade:{bg:"bg-verde",border:"border-verde",text:"text-verde"},Medo:{bg:"bg-lilas",border:"border-lilas",text:"text-lilas"}},h=t=>{const{bg:e,border:a,text:l}=p[t.emocao];return`<div class="w-[85%] lg:w-[60%] p-4 gap-2">
+  `)}function h(){const t=document.querySelector(".search");JSON.parse(localStorage.getItem("desabafos")||"[]").length>0&&(t.classList.remove("hidden"),t.classList.add("flex"));const a=t.querySelector("input");a.addEventListener("input",()=>{const o=(a.value||"").trim().toLowerCase(),l=new CustomEvent("state-change",{detail:{searchText:o}});window.dispatchEvent(l)})}const v={Felicidade:{bg:"bg-amarelo",border:"border-amarelo",text:"text-amarelo"},Tristeza:{bg:"bg-azul",border:"border-azul",text:"text-azul"},Raiva:{bg:"bg-vermelho",border:"border-vermelho",text:"text-vermelho"},Ansiedade:{bg:"bg-laranja",border:"border-laranja",text:"text-laranja"},Motivação:{bg:"bg-rosa",border:"border-rosa",text:"text-rosa"},Tranquilidade:{bg:"bg-verde",border:"border-verde",text:"text-verde"},Medo:{bg:"bg-lilas",border:"border-lilas",text:"text-lilas"}},y=t=>{const{bg:e,border:a,text:o}=v[t.emocao];return`<div class="w-[85%] lg:w-[60%] p-4 gap-2">
             <a class="flex rounded-2xl border-2 ${a} overflow-hidden cursor-pointer hover:scale-105">
                 <div class="lateral ${e} w-3"></div>
                 <div class="content bg-preto flex flex-col flex-1 gap-3 p-2">
                     <div>
-                        <h1 class="${l} font-bold">${t.titulo}</h1>
+                        <h1 class="${o} font-bold">${t.titulo}</h1>
             
                         <p>${t.data}</p>
                     </div>
@@ -108,14 +108,14 @@ import{b}from"./bgParticles-DnaNUedT.js";function f(){const t=document.querySele
                     </div>
                 </div>
             </a>
-        </div>`},y=(t,e,a,l)=>{const o=`<img 
+        </div>`};function w(t,e,a,o){const s=`<img 
                         src="../../public/star.png" 
                         alt="Estrela de intensidade" 
                         class="w-5 h-5"
                     >`.repeat(e.nivel||0);return`
     <div 
         id="${t}" 
-        class="hidden fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/30"
+        class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/30"
     >
         <div 
             class="bg-preto p-6 rounded-2xl shadow-xl w-full max-w-5xl relative border-2 ${a}"
@@ -125,30 +125,30 @@ import{b}from"./bgParticles-DnaNUedT.js";function f(){const t=document.querySele
                 
                 <button 
                     title="Editar"
-                    class="text-gray-400 hover:opacity-80 transition-opacity js-edit-popup"
+                    class="text-gray-400 hover:opacity-80 transition-opacity edit-popup"
                 >
                     <img src="../../public/edit.png" alt="Editar" class="w-5 h-5">
                 </button>
                 
                 <button 
                     title="Remover"
-                    class="text-gray-400 hover:opacity-80 transition-opacity js-remove-popup"
+                    class="text-gray-400 hover:opacity-80 transition-opacity remove-popup"
                 >
-                    <img src="../../public/remove.png" alt="Remover" class="w-5 h-5">
+                    <img src="../../public/trash.png" alt="Remover" class="w-5 h-5">
                 </button>
 
                 <button 
                     title="Fechar"
-                    class="text-gray-400 hover:text-white text-2xl font-bold js-close-popup"
+                    class="text-gray-400 hover:text-white text-2xl font-bold close-popup"
                 >
                     &times;
                 </button>
             </div>
             
-            <h1 class="${l} font-bold text-2xl mb-2">${e.titulo}</h1>
+            <h1 class="${o} font-bold text-2xl mb-2">${e.titulo}</h1>
             
             <div class="flex flex-row gap-x-1 mb-2">
-                ${o}
+                ${s}
             </div>
 
             <p class="text-sm text-gray-400 mb-4">${e.data}</p>
@@ -159,11 +159,11 @@ import{b}from"./bgParticles-DnaNUedT.js";function f(){const t=document.querySele
 
         </div>
     </div>
-    `};function w(t){const{border:e,text:a}=p[t.emocao],l=`modal-${t.id||Date.now()+Math.random()}`,s=y(l,t,e,a);document.body.insertAdjacentHTML("beforeend",s);const o=document.getElementById(l);o.querySelector(".js-close-popup"),o.querySelector(".js-edit-popup"),o.querySelector(".js-remove-popup");const r=o.querySelector(".bg-preto"),i=()=>{o.classList.add("hidden"),o.remove()};o.addEventListener("click",i),r.addEventListener("click",d=>d.stopPropagation()),o.classList.remove("hidden")}function v(t=null,e=null){const a=document.querySelector(".content");a.innerHTML="";const l=t??JSON.parse(localStorage.getItem("desabafos")||"[]");l.length===0&&a.insertAdjacentHTML("afterbegin",`
+    `}function E(t){const{border:e,text:a}=v[t.emocao],o=`modal-${t.id||Date.now()+Math.random()}`;document.body.insertAdjacentHTML("beforeend",w(o,t,e,a));const l=document.getElementById(o),s=l.querySelector(".close-popup"),r=l.querySelector(".edit-popup"),i=l.querySelector(".remove-popup"),m=l.querySelector(".bg-preto");function u(){l.remove()}l.addEventListener("click",u),m.addEventListener("click",d=>{!d.target.closest(".close-popup, .edit-popup, .remove-popup")&&d.stopPropagation()}),s&&s.addEventListener("click",d=>{d.stopPropagation(),u()}),r&&r.addEventListener("click",d=>{d.stopPropagation(),console.log("Editar:",t.id)}),i&&i.addEventListener("click",d=>{d.stopPropagation();let c=JSON.parse(localStorage.getItem("desabafos")||"[]");c=c.filter(b=>b.id!==t.id),localStorage.setItem("desabafos",JSON.stringify(c)),u(),p(c)})}function p(t=null,e=null){const a=document.querySelector(".content");a.innerHTML="";const o=t??JSON.parse(localStorage.getItem("desabafos")||"[]");o.length===0&&a.insertAdjacentHTML("afterbegin",`
         <div class="text-center p-6">
         <p>${e||"Você pode fazer um desabafo agora!"}</p>
         </div>
-  `),l.map(s=>{const o=h(s),r=document.createElement("div");r.innerHTML=o.trim();const i=r.firstElementChild;i&&i.addEventListener("click",()=>{w(s)}),a.prepend(i)})}function E(){const t=document.getElementById("desabafoForm"),e=JSON.parse(localStorage.getItem("desabafos")||"[]");t.addEventListener("submit",a=>{a.preventDefault();const l=new Date,s=String(l.getDate()).padStart(2,"0"),o=String(l.getMonth()+1).padStart(2,"0"),r=l.getFullYear(),i={id:e.length>0?e[e.length-1].id+1:1,titulo:t.titulo.value,emocao:t.emocao.value,nivel:t.nivel.value,descricao:t.descricao.value,data:`${s}/${o}/${r}`};e.push(i),localStorage.setItem("desabafos",JSON.stringify(e)),t.reset(),location.reload()})}function L(){const t=document.getElementById("menu-btn"),e=document.getElementById("sidebar"),a=document.getElementById("close-btn");function l(){e.classList.remove("hidden"),setTimeout(()=>{e.classList.remove("translate-x-full"),e.classList.add("translate-x-0")},10)}function s(){e.classList.add("translate-x-full"),e.classList.remove("translate-x-0"),setTimeout(()=>{e.classList.add("hidden")},300)}t.addEventListener("click",o=>{o.stopPropagation(),l()}),a.addEventListener("click",s),document.addEventListener("click",o=>{!e.contains(o.target)&&!t.contains(o.target)&&s()})}function S(){document.querySelector("#app").insertAdjacentHTML("beforeend",`
+  `),o.map(l=>{const s=y(l),r=document.createElement("div");r.innerHTML=s.trim();const i=r.firstElementChild;i&&i.addEventListener("click",()=>{E(l)}),a.prepend(i)})}function L(){const t=document.getElementById("desabafoForm"),e=JSON.parse(localStorage.getItem("desabafos")||"[]");t.addEventListener("submit",a=>{a.preventDefault();const o=new Date,l=String(o.getDate()).padStart(2,"0"),s=String(o.getMonth()+1).padStart(2,"0"),r=o.getFullYear(),i={id:e.length>0?e[e.length-1].id+1:1,titulo:t.titulo.value,emocao:t.emocao.value,nivel:t.nivel.value,descricao:t.descricao.value,data:`${l}/${s}/${r}`};e.push(i),localStorage.setItem("desabafos",JSON.stringify(e)),t.reset(),location.reload()})}function S(){const t=document.getElementById("menu-btn"),e=document.getElementById("sidebar"),a=document.getElementById("close-btn");function o(){e.classList.remove("hidden"),setTimeout(()=>{e.classList.remove("translate-x-full"),e.classList.add("translate-x-0")},10)}function l(){e.classList.add("translate-x-full"),e.classList.remove("translate-x-0"),setTimeout(()=>{e.classList.add("hidden")},300)}t.addEventListener("click",s=>{s.stopPropagation(),o()}),a.addEventListener("click",l),document.addEventListener("click",s=>{!e.contains(s.target)&&!t.contains(s.target)&&l()})}function T(){document.querySelector("#app").insertAdjacentHTML("beforeend",`
     <div
       id="filter"
       class="bg-roxo border-lilas border-4 rounded-l-lg text-center p-6 shadow-xl fixed bottom-0 right-0 h-full w-85 hidden translate-x-full transform transition-all duration-300 ease-in-out z-50"
@@ -218,4 +218,4 @@ import{b}from"./bgParticles-DnaNUedT.js";function f(){const t=document.querySele
             </button>
         </div>
     </div>
-  `)}function T(){const t=document.querySelector(".emocoes-filter"),e=document.querySelector(".data-filter"),a=[...t.querySelectorAll('input[name="emocao"]:checked')].map(i=>i.value),l=e.querySelector("#date-start").value||null,s=e.querySelector("#date-end").value||null,o={emocoes:a,dateStart:l,dateEnd:s},r=new CustomEvent("state-change",{detail:{filter:o}});window.dispatchEvent(r)}function q(){const t=document.getElementById("filter-btn"),e=document.getElementById("filter"),a=document.getElementById("filter-close-btn"),l=document.getElementById("filter-submit");function s(){e.classList.remove("hidden"),setTimeout(()=>{e.classList.remove("translate-x-full"),e.classList.add("translate-x-0")},10)}function o(){e.classList.add("translate-x-full"),e.classList.remove("translate-x-0"),setTimeout(()=>{e.classList.add("hidden")},300)}t.addEventListener("click",r=>{r.stopPropagation(),s()}),a.addEventListener("click",o),l.addEventListener("click",()=>{T(),o()}),document.addEventListener("click",r=>{!e.contains(r.target)&&!t.contains(r.target)&&o()})}const n={searchText:"",filter:{emocoes:[],dateStart:null,dateEnd:null}};function I(){let e=JSON.parse(localStorage.getItem("desabafos")||"[]");n.filter.emocoes&&n.filter.emocoes.length>0&&(e=e.filter(a=>n.filter.emocoes.includes(a.emocao))),(n.filter.dateStart||n.filter.dateEnd)&&(e=e.filter(a=>{const[l,s,o]=a.data.split("/").map(Number),r=new Date(o,s-1,l),i=n.filter.dateStart?(()=>{const[c,u,m]=n.filter.dateStart.split("-").map(Number);return new Date(c,u-1,m)})():null,d=n.filter.dateEnd?(()=>{const[c,u,m]=n.filter.dateEnd.split("-").map(Number);return new Date(c,u-1,m)})():null;return(!i||r>=i)&&(!d||r<=d)})),n.searchText&&n.searchText.trim()!==""&&(e=e.filter(a=>a.titulo.toLowerCase().includes(n.searchText.toLowerCase().trim())||a.descricao.toLowerCase().includes(n.searchText.toLowerCase().trim()))),v(e,"Nenhum desabafo encontrado.")}window.addEventListener("state-change",t=>{Object.assign(n,{...n,...t.detail}),I()});document.addEventListener("DOMContentLoaded",()=>{b(),f(),v(),E()});g();x();L();S();q();
+  `)}function q(){const t=document.querySelector(".emocoes-filter"),e=document.querySelector(".data-filter"),a=[...t.querySelectorAll('input[name="emocao"]:checked')].map(i=>i.value),o=e.querySelector("#date-start").value||null,l=e.querySelector("#date-end").value||null,s={emocoes:a,dateStart:o,dateEnd:l},r=new CustomEvent("state-change",{detail:{filter:s}});window.dispatchEvent(r)}function k(){const t=document.getElementById("filter-btn"),e=document.getElementById("filter"),a=document.getElementById("filter-close-btn"),o=document.getElementById("filter-submit");function l(){e.classList.remove("hidden"),setTimeout(()=>{e.classList.remove("translate-x-full"),e.classList.add("translate-x-0")},10)}function s(){e.classList.add("translate-x-full"),e.classList.remove("translate-x-0"),setTimeout(()=>{e.classList.add("hidden")},300)}t.addEventListener("click",r=>{r.stopPropagation(),l()}),a.addEventListener("click",s),o.addEventListener("click",()=>{q(),s()}),document.addEventListener("click",r=>{!e.contains(r.target)&&!t.contains(r.target)&&s()})}const n={searchText:"",filter:{emocoes:[],dateStart:null,dateEnd:null}};function I(){let e=JSON.parse(localStorage.getItem("desabafos")||"[]");n.filter.emocoes&&n.filter.emocoes.length>0&&(e=e.filter(a=>n.filter.emocoes.includes(a.emocao))),(n.filter.dateStart||n.filter.dateEnd)&&(e=e.filter(a=>{const[o,l,s]=a.data.split("/").map(Number),r=new Date(s,l-1,o),i=n.filter.dateStart?(()=>{const[u,d,c]=n.filter.dateStart.split("-").map(Number);return new Date(u,d-1,c)})():null,m=n.filter.dateEnd?(()=>{const[u,d,c]=n.filter.dateEnd.split("-").map(Number);return new Date(u,d-1,c)})():null;return(!i||r>=i)&&(!m||r<=m)})),n.searchText&&n.searchText.trim()!==""&&(e=e.filter(a=>a.titulo.toLowerCase().includes(n.searchText.toLowerCase().trim())||a.descricao.toLowerCase().includes(n.searchText.toLowerCase().trim()))),p(e,"Nenhum desabafo encontrado.")}window.addEventListener("state-change",t=>{Object.assign(n,{...n,...t.detail}),I()});document.addEventListener("DOMContentLoaded",()=>{f(),x(),p(),L()});h();g();S();T();k();
