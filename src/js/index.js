c8dac7f5de132658e64d05c2450222140becb6df
Eduardo Brito Oliveira <eduardoerasla22@gmail.com>
2025-11-19 01:46:05 -0300

    Finalizando ladding Page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import bgParticles from "./scripts/bgParticles.js";
+import mobaBar from "./scripts/mobaBar.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     bgParticles();
+
+    mobaBar();
 });
