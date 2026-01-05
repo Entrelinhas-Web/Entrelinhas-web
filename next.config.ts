972740908d7c285484efbab2043f834f1610d7f5
Vitor <lessa.vitorigor@gmail.com>
2026-01-05 09:05:23 -0300

    chore: Adicionando deploy para o NextJS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Entrelinhas-web',
 };
 
 export default nextConfig;
