794793e51425f52f86eac1b2d1cae89fe56724dc
Jairo <jairoccostajr@gmail.com>
2026-01-19 22:20:32 -0300

    fix: atualiza rota do desabafoObject

--- src/app/desabafosFeed/components/desabafoCard.tsx
+++ src/app/desabafosFeed/components/desabafoCard.tsx
@@ -1,4 +1,5 @@
-import { desabafoObject, emocoes } from "../page";
+import { desabafoObject } from "@/src/types/desabafo";
+import { emocoes } from "../page";
 
 export default function DesabafoCard({objeto, onClick}: { objeto: desabafoObject; onClick: () => void}) {
     const { bg, border, text } = emocoes[objeto.emocao];
